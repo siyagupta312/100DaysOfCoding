f9c942acb3eea75767e9c2096cb938b7e6d28b72
siyagupta312 <siyaa.gupta12@gmail.com>
2025-08-28 13:02:37 +0530

    DAY4 Q7.c

--- DAY4 Q7.c
+++ DAY4 Q7.c
@@ -6,12 +6,13 @@ int main() {
     scanf("%d", &a);
   printf("Enter second number: ");
     scanf("%d", &b);
-  a = a + b;  // step 1
-    b = a - b;  // step 2
-    a = a - b;  // step 3
+  a = a + b;  
+    b = a - b;  
+    a = a - b; 
  printf("After swapping:\n");
     printf("First number = %d\n", a);
     printf("Second number = %d\n", b);
 
     return 0;
+
 }
