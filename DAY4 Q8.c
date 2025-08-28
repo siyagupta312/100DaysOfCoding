8ff854121670e60d20071aec90ea5519dadb3ea4
siyagupta312 <siyaa.gupta12@gmail.com>
2025-08-28 14:16:44 +0530

    DAY4 Q8.c

--- DAY4 Q8.c
+++ DAY4 Q8.c
@@ -5,9 +5,10 @@ int main() {
   printf("Enter a positive integer n: ");
     scanf("%d", &n);
  for (int i = 1; i <= n; i++) {
-        sum += i;  // sum = sum + i
+        sum += i;
     }
  printf("The sum of the first %d natural numbers is: %d\n", n, sum);
 
     return 0;
+
 }
