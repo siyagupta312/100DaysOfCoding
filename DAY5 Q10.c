f3851655e5ae3e212ade97c3d308858f3e0dc48a
siyagupta312 <siyaa.gupta12@gmail.com>
2025-08-28 13:12:03 +0530

    DAY5 Q10.c

--- DAY5 Q10.c
+++ DAY5 Q10.c
@@ -5,10 +5,11 @@ int main() {
     int hours, minutes, seconds;
   printf("Enter time in seconds: ");
     scanf("%d", &totalSeconds);
- hours = totalSeconds / 3600;            // 1 hour = 3600 seconds
-    minutes = (totalSeconds % 3600) / 60;  // remaining seconds converted to minutes
-    seconds = totalSeconds % 60;           // remaining seconds
+ hours = totalSeconds / 3600;
+    minutes = (totalSeconds % 3600) / 60;  
+    seconds = totalSeconds % 60;        
  printf("Time = %02d:%02d:%02d (HH:MM:SS)\n", hours, minutes, seconds);
 
     return 0;
 }
+
